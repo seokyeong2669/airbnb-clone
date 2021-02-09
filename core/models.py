4a59df82345dd805a98a4a35af9b63476de91c8f
seokyeong2669 <dltjrdud37@gmail.com>
2021-02-09 23:25:35 +0900

    rooms app

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class TimeStampedModel(models.Model):
     """ Time Stamped Model """
 
     created = models.DateTimeField(auto_now_add=True)  ## auto_now_add : 모델이 생성된 날짜
-    updated = models.DateTimeField(auto_now=True)  ## auto_now : 모델이 저장된 잘짜
+    updated = models.DateTimeField(auto_now=True)  ## auto_now : 모델이 저장된 날짜
 
     class Meta:
         abstract = True
